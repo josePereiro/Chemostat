dc9ac539d235dd12ed5a23499b912e55b8c763f1
josePereiro <jose.pereiro.lab@gmail.com>
2020-04-23 20:12:38 -0400

    Changes in plot_marginals

--- src/Plots/Plots.jl
+++ src/Plots/Plots.jl
@@ -2,11 +2,13 @@
     A few useful plot methods
 """
 module Plots
-    import ..Utils: rxnindex, marginal, rxns, lb, ub, HRout, hists, μ, FBAout, pdf_maxval
+    import ..Utils: logspace, rxnindex, marginal, rxns, lb, ub, HRout, hists, μ, σ, FBAout, pdf_maxval
     import MetabolicEP: EPout, MetNet
     import Distributions: mean, var, pdf
     import LinearAlgebra: normalize
     import Plots: plot, plot!, scatter, scatter!, histogram, histogram!
 
     include("plot_marginal.jl")
+    include("normal_xs_for_plotting.jl")
 end
+
--- src/Plots/plot_marginal.jl
+++ src/Plots/plot_marginal.jl
@@ -2,26 +2,30 @@ const ep_color = :red
 const fba_color = :blue
 const hr_color = :black
 
-
+# Single out
 function plot_marginal!(p, metnet::MetNet, out::Union{FBAout, EPout}, ider; h = 1,
     color = :black, label = rxns(metnet, ider), kwargs...)
-    kwargs = Dict(kwargs)
 
     lb_ = lb(metnet, ider)
     ub_ = ub(metnet, ider)
 
     dist = marginal(metnet, out, ider)
-    if var(dist) < (ub_ - lb_)/50
+
+    if sqrt(var(dist)) == 0.0
         plot!(p, [lb_ - ub_/10, ub_ + ub_/10], [0.0, 0.0]; 
             label = "", color = color, kwargs...)
         plot!(p, [mean(dist), mean(dist)], [0.0, h]; 
             label = label, color = color, kwargs...)
     else
-        plot!(p, x-> pdf(dist, x), lb_ - ub_/10, ub_ + ub_/10;
-                    label = label, color = color, kwargs...)
+        μ_ = μ(metnet, out, ider);
+        σ_ = σ(metnet, out, ider);
+        xs = normal_xs_for_plotting(μ_, σ_, lb_ - ub_/10, ub_ + ub_/10)
+        ys = [pdf(dist, x) for x in xs]
+        plot!(p, xs, ys; label = label, color = color, kwargs...)
     end
 
 end
+
 
 function plot_marginal!(p, metnet::MetNet, out::HRout, ider; 
     color = :black, label = rxns(metnet, ider), kwargs...)
@@ -30,12 +34,14 @@ function plot_marginal!(p, metnet::MetNet, out::HRout, ider;
     ub_ = ub(metnet, ider)
 
     hist = hists(metnet, out, ider)
-    plot!(p,normalize(hist, mode = :pdf); 
+    plot!(p, normalize(hist, mode = :pdf); 
         label = label, color = color, kwargs...)
 end
 
+# 3 outs
 function plot_marginal!(p, metnet::MetNet, 
-        hrout::HRout, epout::EPout, fbaout::FBAout, ider)
+        fbaout::FBAout, epout::EPout, hrout::HRout, 
+        ider::Union{AbstractString, Integer})
 
     pdf_maxval_ = pdf_maxval(metnet, [hrout, epout], ider)
     plot_marginal!(p, metnet, hrout, ider; color = hr_color, label = "HR")
@@ -44,27 +50,57 @@ function plot_marginal!(p, metnet::MetNet,
     ls = :dash,  color = fba_color,  lw = 5, label = "FBA")
 end
 
-function plot_marginal(metnet::MetNet, hrout::HRout, epout::EPout, fbaout::FBAout, 
-        ider::Union{AbstractString, Integer})
-    p = Plots.plot(title = "$(rxns(metnet, ider))", xlabel = "flx", ylabel = "pdf")
-    plot_marginal!(p, metnet, hrout, epout, fbaout, ider)
-    return p
+function plot_marginal(metnet::MetNet, 
+        fbaout::FBAout, epout::EPout, hrout::HRout, 
+        ider::Union{AbstractString, Integer};
+        title = rxns(metnet, ider), xlabel = "flx", ylabel = "pdf")
+
+        p = plot(title = title, xlabel = xlabel, ylabel = ylabel)
+        return plot_marginal!(p, metnet, fbaout, epout, hrout, ider)
 end
 
-plot_marginal(metnet::MetNet, hrout::HRout, epout::EPout, fbaout::FBAout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
+plot_marginal(metnet::MetNet, 
+        fbaout::FBAout, epout::EPout, hrout::HRout, 
+        iders) = [plot_marginal(metnet, fbaout, epout, hrout, ider) for ider in iders]
 
-plot_marginal(metnet::MetNet, hrout::HRout, fbaout::FBAout, epout::EPout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
 
-plot_marginal(metnet::MetNet, fbaout::FBAout, epout::EPout, hrout::HRout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
 
-plot_marginal(metnet::MetNet, fbaout::FBAout, hrout::HRout, epout::EPout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
+# 2 outs
+function plot_marginal!(p, metnet::MetNet, 
+    fbaout::EPout, epout::HRout,
+    ider::Union{AbstractString, Integer})
 
-plot_marginal(metnet::MetNet, epout::EPout, fbaout::FBAout, hrout::HRout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
+    pdf_maxval_ = pdf_maxval(metnet, [epout], ider)
+    plot_marginal!(p, metnet, epout, ider; color = ep_color, lw = 10, label = "EP")
+    plot_marginal!(p, metnet, fbaout, ider; h = pdf_maxval_ * 1.1,
+        ls = :dash,  color = fba_color,  lw = 5, label = "FBA")
+end
 
-plot_marginal(metnet::MetNet, epout::EPout, hrout::HRout, fbaout::FBAout, iders) = 
-    [plot_marginal(metnet, hrout, epout, fbaout, ider) for ider in iders]
+function plot_marginal!(p, metnet::MetNet, 
+    fbaout::FBAout, hrout::HRout,
+    ider::Union{AbstractString, Integer})
+
+    pdf_maxval_ = pdf_maxval(metnet, [hrout], ider)
+    plot_marginal!(p, metnet, hrout, ider; color = hr_color, label = "HR")
+    plot_marginal!(p, metnet, fbaout, ider; h = pdf_maxval_ * 1.1,
+        ls = :dash,  color = fba_color,  lw = 5, label = "FBA")
+end
+
+function plot_marginal!(p, metnet::MetNet, 
+        epout::EPout, hrout::HRout,
+        ider::Union{AbstractString, Integer})
+
+    plot_marginal!(p, metnet, hrout, ider; color = hr_color, label = "HR")
+    plot_marginal!(p, metnet, epout, ider; color = ep_color, lw = 10, label = "EP")
+end
+
+function plot_marginal(metnet::MetNet, out1, out2,
+        ider::Union{AbstractString, Integer},
+        title = rxns(metnet, ider), xlabel = "flx", ylabel = "pdf")
+
+    p = plot(title = title, xlabel = xlabel, ylabel = ylabel)
+    return plot_marginal!(p, metnet, out1, out2, ider)
+end
+
+plot_marginal(metnet::MetNet, out1, out2,
+        iders) = [plot_marginal(metnet, out1, out2, ider) for ider in iders]
