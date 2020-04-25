53387a5018d58159a3708b6e7994befc23e6c836
josePereiro <jose.pereiro.lab@gmail.com>
2020-04-25 15:51:51 -0400

    Fix bug in stoi_err

--- src/Utils/OutUtils/stoi_err.jl
+++ src/Utils/OutUtils/stoi_err.jl
@@ -1,8 +1,8 @@
 stoi_err(S, v, b) = S * v - b
 
-av_stoi_err(metnet::MetNet, out) = stoi_err(metnet.S, va(out), metnet.b)
+av_stoi_err(metnet::MetNet, out) = stoi_err(metnet.S, av(out), metnet.b)
 av_stoi_err(metnet::MetNet, out, ider) = 
-    stoi_err(metnet.S, va(out), metnet.b)[metindex(metnet, ider)]
+    stoi_err(metnet.S, av(out), metnet.b)[metindex(metnet, ider)]
 
 av_stoi_err_ep(bound::ChstatBoundle, ξ::Real, β::Real) = 
     av_stoi_err(get_metnet(bound, ξ), get_epout(bound, ξ, β))
