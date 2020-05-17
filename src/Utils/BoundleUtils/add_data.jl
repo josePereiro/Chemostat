2a29ac505ba5ba6cf4d1c6779152da109f68f784
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-17 14:00:22 -0400

    Add warnings for common data

--- src/Utils/BoundleUtils/add_data.jl
+++ src/Utils/BoundleUtils/add_data.jl
@@ -1,41 +1,39 @@
-function add_data!(boundle::ChstatBoundle, ξ::Real, β::Real, key::AbstractString, data)
+function add_data!(boundle::ChstatBoundle, ξ::Real, β::Real, data_key::Symbol, data)
+    check_data_key(data_key, data)
     k_tuple = (ξ, β)
     if !haskey(boundle.data, k_tuple)
         boundle.data[k_tuple] = Dict()
     end
-    boundle.data[k_tuple][key] = data
+    boundle.data[k_tuple][data_key] = data
 
     !(ξ in boundle.ξs) && push!(boundle.ξs, ξ)
     !(β in boundle.βs) && push!(boundle.βs, β)
 end
-
-# add_epout!(boundle::ChstatBoundle, ξ::Real, β::Real, epout::EPout) = 
-#     add_data!(boundle, ξ, β, epout_key, epout)
-
-# add_epmat!(boundle::ChstatBoundle, ξ::Real, β::Real, epmat::AbstractEPMat) = 
-#     add_data!(boundle, ξ, β, epmat_key, epmat)
-
-# add_hrout!(boundle::ChstatBoundle, ξ::Real, β::Real, hrout::HRout) = 
-#     add_data!(boundle, ξ, β, hrout_key, hrout)
  
-function add_data!(boundle::ChstatBoundle, ξ::Real, key::AbstractString, data)
+function add_data!(boundle::ChstatBoundle, ξ::Real, data_key::Symbol, data)
+    check_data_key(data_key, data)
     if !haskey(boundle.data, ξ)
         boundle.data[ξ] = Dict()
     end
-    boundle.data[ξ][key] = data
+    boundle.data[ξ][data_key] = data
 
     !(ξ in boundle.ξs) && push!(boundle.ξs, ξ)
 end
 
-# add_fbaout!(boundle::ChstatBoundle, ξ::Real, fbaout::FBAout) = 
-#     add_data!(boundle, ξ, fbaout_key, fbaout)
-
-# add_metnet!(boundle::ChstatBoundle, ξ::Real, metnet::MetNet) = 
-#     add_data!(boundle, ξ, metnet_key, metnet)
-
-function add_data!(boundle::ChstatBoundle, key::AbstractString, data)
-    if !haskey(boundle.data, key)
-        boundle.data[key] = Dict()
+function add_data!(boundle::ChstatBoundle, data_key::Symbol, data)
+    check_data_key(data_key, data)
+    if !haskey(boundle.data, data_key)
+        boundle.data[data_key] = Dict()
     end
-    boundle.data[key] = data
+    boundle.data[data_key] = data
+end
+
+# Warn about using non default keys for common data
+function check_data_key(data_key, data)
+    _warn(default_key) = 
+        data_key != default_key && @warn("Not using default data_key ($default_key)!!!")
+    data isa EPout ? _warn(epout_data_key) :
+    data isa FBAout ? _warn(fbaout_data_key) :
+    data isa HRout ? _warn(hrout_data_key) :
+    data isa MetNet ? _warn(metnet_data_key) : nothing
 end
