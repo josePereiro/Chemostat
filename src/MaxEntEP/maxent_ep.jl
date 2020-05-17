b9334fce747ee3afd7b3b2298077d005cc1feb75
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-17 04:21:56 -0400

    Change maxent_ep beta_v interface

--- src/MaxEntEP/maxent_ep.jl
+++ src/MaxEntEP/maxent_ep.jl
@@ -1,9 +1,8 @@
 # Code derived from metabolicEP (https://github.com/anna-pa-m/Metabolic-EP)
 
 function maxent_ep(K::AbstractArray{T,2}, Y::Array{T,1}, lb::Array{T,1}, ub::Array{T,1};
-    beta::Real=1e7,      # inverse temperature
-    obj_idx::Integer = -1, # maxent objective flx index
-    beta_maxent::Real = 0.0, # maxenet inverse temperature
+    α::Real=1e7,      # inverse temperature
+    βv::Vector{T} = T[], # maxent inverse temperature vector
     verbose::Bool=true,  # output verbosity
     damp::Real=0.9,      # damp ∈ (0,1) newfield = damp * oldfield + (1-damp)* newfield
     epsconv::Real=1e-6,  # convergence criterion
@@ -15,38 +14,38 @@ function maxent_ep(K::AbstractArray{T,2}, Y::Array{T,1}, lb::Array{T,1}, ub::Arr
     ) where T<:Real
 
 
-llb = copy(lb) # making  a local copy to rescale
-lub = copy(ub)
+    llb = copy(lb) # making  a local copy to rescale
+    lub = copy(ub)
 
-#= 
-Create EPField, set scale factor and return updatealg (updatefunction),
-depending on beta (eponesweepT0! or eponesweep).
-The scalefactor is just the maximum absolute bound (lb or ub).
-If a solution is not given, the EPfield will be fresh
-=#
-updatealg,scalefact,epfield = prepareinput(K,Y,llb,lub,beta,verbose,solution,expval)
+    #= 
+    Create EPField, set scale factor and return updatealg (updatefunction),
+    depending on beta (eponesweepT0! or eponesweep).
+    The scalefactor is just the maximum absolute bound (lb or ub).
+    If a solution is not given, the EPfield will be fresh
+    =#
+    updatealg,scalefact,epfield = prepareinput(K,Y,llb,lub, α, verbose,solution,expval)
 
-#=
-Scale down μ, s, av, va of epfield and lub, llb and Y using the 
-previous computed scalefactor.
-If epfield is fresh, it only will have effect on lub, llb and Y
-=#
-scaleepfield!(epfield,lub,llb,Y,1.0/scalefact) # rescaling fields in [0,1]
+    #=
+    Scale down μ, s, av, va of epfield and lub, llb and Y using the 
+    previous computed scalefactor.
+    If epfield is fresh, it only will have effect on lub, llb and Y
+    =#
+    scaleepfield!(epfield,lub,llb,Y,1.0/scalefact) # rescaling fields in [0,1]
 
-epmat = beta < Inf ? EPMat(K,Y,llb, lub, beta) : EPMatT0(K,Y,llb, lub)
-beta_mv = prepare_maxent_beta_v(epmat, beta_maxent, obj_idx)
-epalg = EPAlg(beta, beta_mv, minvar, maxvar, epsconv, damp, maxiter, verbose)
+    epmat = α < Inf ? EPMat(K, Y, llb, lub, α) : EPMatT0(K,Y,llb, lub)
+    βv = prepare_βv(epmat, βv)
+    epalg = EPAlg(α, βv, minvar, maxvar, epsconv, damp, maxiter, verbose)
 
-#= Here is were all the work is done, this function will 
-call updatealg till convergence or maxiter is reached =#
-returnstatus = epconverge!(epfield, epmat, epalg, updatealg)
+    #= Here is were all the work is done, this function will 
+    call updatealg till convergence or maxiter is reached =#
+    returnstatus = epconverge!(epfield, epmat, epalg, updatealg)
 
-#= Scale back μ, s, av, va of epfield and lub, llb and Y =#
-scaleepfield!(epfield,lub,llb,Y,scalefact)
-if beta < Inf
-return  EPout(epfield.μ,epfield.s, epfield.av, epfield.va, epfield, returnstatus)
-else
-idx = epmat.idx
-return  EPout(epfield.μ[idx],epfield.s[idx],epfield.av[idx],epfield.va[idx], epfield, returnstatus)
-end
+    #= Scale back μ, s, av, va of epfield and lub, llb and Y =#
+    scaleepfield!(epfield,lub,llb,Y,scalefact)
+    if α < Inf
+        return  EPout(epfield.μ,epfield.s, epfield.av, epfield.va, epfield, returnstatus)
+    else
+        idx = epmat.idx
+        return  EPout(epfield.μ[idx],epfield.s[idx],epfield.av[idx],epfield.va[idx], epfield, returnstatus)
+    end
 end
---
+++ src/MaxEntEP/prepare_beta_v.jl
@@ -0,0 +1,20 @@
+# Code derived from metabolicEP (https://github.com/anna-pa-m/Metabolic-EP)
+
+function prepare_βv(epmat::EPMat, βv::Vector)
+    N = length(epmat.v)
+    isempty(βv) && return zeros(N)
+    length(βv) != N && error("βv lenght != $N")
+    !all(βv .> zero(eltype(βv))) && error("maxent betas must be positive!!!")
+    return copy(βv)
+end
+
+function prepare_βv(epmat::EPMatT0, βv::Vector)
+        M = length(epmat.vy) # Number of dependent variables
+        N = length(epmat.vy) + length(epmat.vw)
+
+        isempty(βv) && return zeros(N - M) # same length as independent variables
+        length(βv) != N && error("βv lenght != $N")
+        !all(βv .> zero(eltype(βv))) && error("maxent betas must be positive!!!")
+        return βv[epmat.idx[M + 1:end]]
+end
+
