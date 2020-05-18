ed1d6c78148711f08060e28580d0366aa614eefb
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-18 17:24:13 -0400

    make MetNet typed <:Real

--- src/Utils/Types/MetNet.jl
+++ src/Utils/Types/MetNet.jl
@@ -5,22 +5,22 @@
     and the ** the relevante for other methods in this package,
     the rest are untouched, so they may be contain dummy values sometimes!!!
 =#
-struct MetNet
-    N::Int # number of fluxes
-    M::Int # number of metabolites
-    S::SparseMatrixCSC{Float64,Int} # Stoichiometric matrix M x N sparse (*)
-    b::Array{Float64,1} # right hand side of equation  S ν = b (*)
-    c::Array{Float64,1} # reaction index of biomass 
-    lb::Array{Float64,1} # fluxes lower bound M elements vector (*)
-    ub::Array{Float64,1} # fluxes upper bound M elements vector (*)
-    genes::Array{String,1} # gene names 
-    rxnGeneMat::SparseMatrixCSC{Float64,Int} # 
-    grRules::Array{String,1} # gene-reaction rule N elements vector of strings (and / or allowed)
-    mets::Array{String,1} # metabolites short-name M elements (**)
-    rxns::Array{String,1} # reactions short-name N elements (*)
-    metNames::Array{String,1} # metabolites long-names M elements (**)
-    metFormulas::Array{String,1} # metabolites formula M elements (**)
-    rxnNames::Array{String,1} # reactions long-names N elements (**)
-    rev::Array{Bool,1} # reversibility of reactions N elements
-    subSystems::Array{String,1} # cellular component of fluxes N elements
-end;
+struct MetNet{T<:Real}
+    N::Integer # number of fluxes
+    M::Integer # number of metabolites
+    S::AbstractMatrix{T} # Stoichiometric matrix M x N sparse (*)
+    b::AbstractArray{T} # right hand side of equation  S ν = b (*)
+    c::AbstractArray{Float64} # reaction index of biomass 
+    lb::AbstractArray{T} # fluxes lower bound M elements vector (*)
+    ub::AbstractArray{T} # fluxes upper bound M elements vector (*)
+    genes::AbstractArray{String} # gene names 
+    rxnGeneMat::AbstractMatrix{Float64} # 
+    grRules::AbstractArray{String} # gene-reaction rule N elements vector of strings (and / or allowed)
+    mets::AbstractArray{String} # metabolites short-name M elements (**)
+    rxns::AbstractArray{String} # reactions short-name N elements (*)
+    metNames::AbstractArray{String} # metabolites long-names M elements (**)
+    metFormulas::AbstractArray{String} # metabolites formula M elements (**)
+    rxnNames::AbstractArray{String} # reactions long-names N elements (**)
+    rev::AbstractArray{Bool} # reversibility of reactions N elements
+    subSystems::AbstractArray{String} # cellular component of fluxes N elements
+end
