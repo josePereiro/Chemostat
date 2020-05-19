5c5bc397b3305430bf7688630279b15b723086fb
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-19 03:54:33 -0400

    Update add_mets

--- src/Utils/BoundleUtils/metnet_json.jl
+++ src/Utils/BoundleUtils/metnet_json.jl
@@ -1,47 +1,47 @@
-function metnet_to_json(metnet::MetNet)
-    data_dict = Dict()
-    data_dict["N"] = metnet.N
-    data_dict["M"] = metnet.M
-    data_dict["S"] = findnz(sparse(metnet.S))
-    data_dict["b"] = findnz(sparsevec(metnet.b))
-    data_dict["c"] = findnz(sparsevec(metnet.c))
-    data_dict["lb"] = findnz(sparsevec(metnet.lb))
-    data_dict["ub"] = metnet.ub # ub use to be non-dense
-    data_dict["genes"] = metnet.genes
-    data_dict["rxnGeneMat"] = findnz(sparse(metnet.rxnGeneMat))
-    data_dict["grRules"] = metnet.grRules
-    data_dict["mets"] = metnet.mets
-    data_dict["rxns"] = metnet.rxns
-    data_dict["metNames"] = metnet.metNames
-    data_dict["metFormulas"] = metnet.metFormulas
-    data_dict["rxnNames"] = metnet.rxnNames
-    data_dict["rev"] = "Should be reconstructed from bounds"
-    data_dict["subSystems"] = metnet.subSystems
-    return json(data_dict)
-end
+# function metnet_to_json(metnet::MetNet)
+#     data_dict = Dict()
+#     data_dict["N"] = metnet.N
+#     data_dict["M"] = metnet.M
+#     data_dict["S"] = findnz(sparse(metnet.S))
+#     data_dict["b"] = findnz(sparsevec(metnet.b))
+#     data_dict["c"] = findnz(sparsevec(metnet.c))
+#     data_dict["lb"] = findnz(sparsevec(metnet.lb))
+#     data_dict["ub"] = metnet.ub # ub use to be non-dense
+#     data_dict["genes"] = metnet.genes
+#     data_dict["rxnGeneMat"] = findnz(sparse(metnet.rxnGeneMat))
+#     data_dict["grRules"] = metnet.grRules
+#     data_dict["mets"] = metnet.mets
+#     data_dict["rxns"] = metnet.rxns
+#     data_dict["metNames"] = metnet.metNames
+#     data_dict["metFormulas"] = metnet.metFormulas
+#     data_dict["rxnNames"] = metnet.rxnNames
+#     data_dict["rev"] = "Should be reconstructed from bounds"
+#     data_dict["subSystems"] = metnet.subSystems
+#     return json(data_dict)
+# end
 
-# TODO check this for improvement
-function metnet_from_json(json_str)
-    data_dict = parse(json_str)
-    N = Int(data_dict["N"])
-    M = Int(data_dict["M"])
-    S = sparse(data_dict["S"]..., M, N)
-    b = collect(SparseVector{Float64, Int}(sparsevec(data_dict["b"]..., M)))
-    c = collect(SparseVector{Float64, Int}(sparsevec(data_dict["c"]..., N)))
-    lb = collect(SparseVector{Float64, Int}(sparsevec(data_dict["lb"]..., N)))
-    ub = Vector{Float64}(data_dict["ub"])
-    genes = Vector{String}(data_dict["genes"])
-    rxnGeneMat = sparse(data_dict["rxnGeneMat"]..., M, N)
-    grRules = data_dict["grRules"]
-    mets = data_dict["mets"]
-    rxns = data_dict["rxns"]
-    metNames = data_dict["metNames"]
-    metFormulas = data_dict["metFormulas"]
-    rxnNames = data_dict["rxnNames"]
-    rev = (lb .< 0.0) .& (lb .> 0.0)
-    subSystems = data_dict["subSystems"]
+# # TODO check this for improvement
+# function metnet_from_json(json_str)
+#     data_dict = parse(json_str)
+#     N = Int(data_dict["N"])
+#     M = Int(data_dict["M"])
+#     S = sparse(data_dict["S"]..., M, N)
+#     b = collect(SparseVector{Float64, Int}(sparsevec(data_dict["b"]..., M)))
+#     c = collect(SparseVector{Float64, Int}(sparsevec(data_dict["c"]..., N)))
+#     lb = collect(SparseVector{Float64, Int}(sparsevec(data_dict["lb"]..., N)))
+#     ub = Vector{Float64}(data_dict["ub"])
+#     genes = Vector{String}(data_dict["genes"])
+#     rxnGeneMat = sparse(data_dict["rxnGeneMat"]..., M, N)
+#     grRules = data_dict["grRules"]
+#     mets = data_dict["mets"]
+#     rxns = data_dict["rxns"]
+#     metNames = data_dict["metNames"]
+#     metFormulas = data_dict["metFormulas"]
+#     rxnNames = data_dict["rxnNames"]
+#     rev = (lb .< 0.0) .& (lb .> 0.0)
+#     subSystems = data_dict["subSystems"]
     
-    return MetNet(S,b,c,lb,ub,genes,
-        rxnGeneMat,grRules,mets,rxns,metNames,
-        metFormulas,rxnNames,rev,subSystems)
-end
+#     return MetNet(S,b,c,lb,ub,genes,
+#         rxnGeneMat,grRules,mets,rxns,metNames,
+#         metFormulas,rxnNames,rev,subSystems)
+# end
--- src/Utils/MetNetUtils/add_met.jl
+++ src/Utils/MetNetUtils/add_met.jl
@@ -1,4 +1,3 @@
-# TODO fully implement this!!!
 """
     Returns a metnet with the added metabolite metid.
 
@@ -14,37 +13,19 @@ function add_met(metnet::MetNet, metid::AbstractString; kwargs...)
     kwargs = Dict(kwargs)
     
     M, N = size(metnet)
-    M_, N_ = M + 1, N
-    S_ = spzeros(M_, N_)
+    S_ = spzeros(M + 1, N)
     S_[1:M,1:N] .= metnet.S
-    b_ = copy(metnet.b)
-    c_ = copy(metnet.c)
-    lb_ = copy(metnet.lb)
-    ub_ = copy(metnet.ub)
-    genes_ = copy(metnet.genes)
-    rxnGeneMat_ = copy(metnet.rxnGeneMat)
-    grRules_ = copy(metnet.grRules)
-    mets_ = copy(metnet.mets)
-    rxns_ = copy(metnet.rxns)
-    metNames_ = copy(metnet.metNames)
-    metFormulas_ = copy(metnet.metFormulas)
-    rxnNames_ = copy(metnet.rxnNames)
-    rev_ = copy(metnet.rev)
-    subSystems_ = copy(metnet.subSystems)
-    
     rxns_info = Dict(get(kwargs, :rxns, Dict()))
     for (rxn, stoi_coe) in rxns_info
         rxnidx = rxnindex(metnet, rxn)
         S_[end, rxnidx] = stoi_coe
     end
-    
-    push!(mets_, metid)
-    push!(metNames_, get(kwargs, :metName, metNames_default))
-    push!(metFormulas_, get(kwargs, :metFormula, metFormulas_default))
-    push!(b_, get(kwargs, :b, b_default))
 
+    b_ = vcat(metnet.b, get(kwargs, :b, b_default))
+    mets_ = vcat(metnet.mets, metid)
+    metNames_ = vcat(metnet.metNames, get(kwargs, :metName, metNames_default))
+    metFormulas_ = vcat(metnet.metFormulas, get(kwargs, :metFormula, metFormulas_default))
     
-    return MetNet(S_,b_, c_, lb_, ub_, 
-        genes_, rxnGeneMat_, grRules_, mets_, rxns_, 
-        metNames_, metFormulas_, rxnNames_, rev_, subSystems_)
+    return MetNet(metnet, S = S_, b = b_, mets = mets_, 
+                    metNames = metNames_, metFormulas = metFormulas_)
 end
