3afad1e0dac4b798f1bf67a60c091f3695f2615c
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-29 16:53:04 -0400

    Update stoi_err

--- src/Utils/BoundleUtils/stoi_err.jl
+++ src/Utils/BoundleUtils/stoi_err.jl
@@ -1,14 +1,18 @@
-av_stoi_err_ep(bound::ChstatBoundle, ξ::Real, β::Real) = 
-    av_stoi_err(get_metnet(bound, ξ), get_epout(bound, ξ, β))
-av_stoi_err_ep(bound::ChstatBoundle, ξ::Real, β::Real, ider) = 
-    av_stoi_err(get_metnet(bound, ξ), get_epout(bound, ξ, β), ider)
+stoi_err(bound::ChstatBoundle, ξ::Real, β::Real, data_key::Symbol, 
+            metnet_data_key::Symbol = metnet_data_key) = 
+    stoi_err(get_data(bound, ξ, metnet_data_key), get_data(bound, ξ, β, data_key))
 
-av_stoi_err_fba(bound::ChstatBoundle, ξ::Real) = 
-    av_stoi_err(get_metnet(bound, ξ), get_fbaout(bound, ξ))
-av_stoi_err_fba(bound::ChstatBoundle, ξ::Real, ider) = 
-    av_stoi_err(get_metnet(bound, ξ), get_fbaout(bound, ξ), ider)
 
-av_stoi_err_hr(bound::ChstatBoundle, ξ::Real, β::Real) = 
-    av_stoi_err(get_metnet(bound, ξ), get_hrout(bound, ξ, β))
-av_stoi_err_hr(bound::ChstatBoundle, ξ::Real, β::Real, ider) = 
-    av_stoi_err(get_metnet(bound, ξ), get_hrout(bound, ξ, β), ider)
+stoi_err(bound::ChstatBoundle, ξ::Real, data_key::Symbol, 
+        metnet_data_key::Symbol = metnet_data_key) = 
+    stoi_err(get_data(bound, ξ, metnet_data_key), get_data(bound, ξ, data_key))
+
+
+
+stoi_err(bound::ChstatBoundle, ξ::Real, β::Real, data_key::Symbol, ider::IDER_TYPE,
+        metnet_data_key::Symbol = metnet_data_key) = 
+    stoi_err(get_data(bound, ξ, metnet_data_key), get_data(bound, ξ, β, data_key),  ider)
+
+stoi_err(bound::ChstatBoundle, ξ::Real, data_key::Symbol, ider::IDER_TYPE,
+        metnet_data_key::Symbol = metnet_data_key) = 
+    stoi_err(get_data(bound, ξ, metnet_data_key), get_data(bound, ξ, data_key),  ider)
--- src/Utils/OutUtils/stoi_err.jl
+++ src/Utils/OutUtils/stoi_err.jl
@@ -1,6 +1,6 @@
 stoi_err(S, v, b) = S * v - b
 
-av_stoi_err(metnet::MetNet, out) = stoi_err(metnet.S, av(out), metnet.b)
-av_stoi_err(metnet::MetNet, out, ider) = 
+stoi_err(metnet::MetNet, out) = stoi_err(metnet.S, av(out), metnet.b)
+stoi_err(metnet::MetNet, out, ider) = 
     stoi_err(metnet.S, av(out), metnet.b)[metindex(metnet, ider)]
 
