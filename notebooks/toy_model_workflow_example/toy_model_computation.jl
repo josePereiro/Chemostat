c00e19548ef9fa8841785db86f86a9eb2d780aed
josePereiro <jose.pereiro.lab@gmail.com>
2020-05-31 01:52:21 -0400

    Toy_Model parallel workflow implemented

---
+++ notebooks/toy_model_workflow_example/toy_model_computation.jl
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: jl,ipynb
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Julia 1.1.0
+#     language: julia
+#     name: julia-1.1
+# ---
+
+@everywhere begin
+    obj_ider = "biom";
+    α = 1e9
+    intake_info = Dict("gt" => Dict("ub" => 100.0, "c" => 10.0));
+
+    ξs = Ch.Utils.logspace(-1,1,20);
+    ξs_str = round.(ξs, digits = 2);
+
+    β0 = 0.0;
+    βs = [β0; Ch.Utils.logspace(-1,3, 20)] # This is the working interval of HR
+    βs_str = round.(βs, digits = 2);
+end
+
+# ---
+# ## EP, FBA, HR
+# ---
+
+# ### Initializing everywhere
+
+println("α: ", α)
+println("βs: ", βs_str)
+println("ξs: ", ξs_str)
+
+# ### work function
+
+@everywhere function process_xi(ξ)
+    
+    println("Doing xi: $ξ"); flush(stdout)
+    
+    data = Dict()
+    
+    # Preparing model
+    model = Ch.Utils.toy_model();
+    model = Ch.SteadyState.apply_bound!(model, ξ, intake_info)
+    model = Ch.Utils.fva_preprocess(model)
+    obj_idx = Ch.Utils.rxnindex(model, obj_ider)
+
+    # fba
+    fbaout = Ch.FBA.fba(model, obj_ider)
+    
+    # storing
+    data[(ξ, :net)] = model
+    data[(ξ, :fba)] = fbaout   
+
+    βv = zeros(size(model, 2))
+    seed_epout = nothing
+    for (βi, β) in enumerate(βs)
+        
+        # hrout
+        hrout = Ch.MaxEntHR.maxent_hr(model, obj_ider, 
+            β * 10.0^(-sqrt(ℯ)); # I have no idea why these work! TODO find it!
+            nsamples = 10_000)
+
+        # epout
+        βv[obj_idx] = β
+        epout = Ch.MaxEntEP.maxent_ep(model, α = α, βv = βv, 
+            epsconv = 1e-10, # This is an exageration!!
+            solution = seed_epout, verbose = false)
+        seed_epout = epout
+        
+        # storing
+        data[(ξ, β, :ep)] = epout
+        data[(ξ, β, :hr)] = hrout
+
+    end # β loop
+    
+    return data
+end
+
+function boundle_data!(boundle, ξ, βs, data)
+
+    Ch.Utils.add_data!(boundle, ξ, :net, data[(ξ, :net)])
+    Ch.Utils.add_data!(boundle, ξ, :fba, data[(ξ, :fba)])
+    
+    for β in βs
+        Ch.Utils.add_data!(boundle, ξ, β, :ep, data[(ξ, β, :ep)])
+        Ch.Utils.add_data!(boundle, ξ, β, :hr, data[(ξ, β, :hr)])
+    end
+end
+
+# ### Parallel loop
+
+# +
+# this can take a while!!!
+boundle = Ch.Utils.ChstatBoundle()
+
+ξs_ = copy(ξs)
+while !isempty(ξs_)
+    @sync for w in workers()
+        @async begin
+            if !isempty(ξs_)
+                ξ = pop!(ξs_)
+                data = fetch(@spawnat w process_xi(ξ))
+                boundle_data!(boundle, ξ, βs, data)
+                flush(stdout)
+            end
+        end
+    end
+end # ξ loop
+
+println("Done!!!", " "^100);
+# -
+
+### Catching
+cache_file = "toy_model_cache.jls"
+serialize(cache_file, boundle)
+println(relpath(cache_file), " created!!!")
---
+++ notebooks/toy_model_workflow_example/toy_model_plots.jl
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: jl,ipynb
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Julia 1.1.0
+#     language: julia
+#     name: julia-1.1
+# ---
+
+using StatsBase
+using Serialization
+using Plots
+
+# run add "https://github.com/josePereiro/Ch" in the julia Pkg REPL for installing the package
+import Chemostat
+Ch = Chemostat;
+
+# ### Loading cache
+# This notebook use the cached data from [toy_model_computation.ipynb](./toy_model_computation.ipynb)
+
+cache_file = "../../dev/toy_model_cache.jls"#"toy_model_cache.jls"
+boundle = deserialize(cache_file)
+sort!(boundle.ξs)
+sort!(boundle.βs)
+println(cache_file, " loaded!!!")
+
+# ---
+# ## Plots
+# --- 
+
+# ### Marginals
+
+# +
+# Plot the marginals as function of βs
+
+# An increase β implies that the solutions with greater value of the objective reaction (biom in this case)
+# will be more probables (more prob density around the large values of biom). The rest of the fluxes will change 
+# depending in its covarianze with biom
+
+ξ = boundle.ξs[4] # Select ξ to plot
+metnet = Ch.Utils.get_data(boundle, ξ, :net)
+fbaout = Ch.Utils.get_data(boundle, ξ, :fba)
+ps = []
+βs_ = boundle.βs[2:end] # Select βs to plot
+βs_ = [βs_; reverse(βs_)]
+iders_ = metnet.rxns
+
+function plot1(iders, ξ, β)
+    ps = []
+    for ider in iders
+        ξstr = round(ξ, digits = 1)
+        βstr = round(β, digits = 1)
+        p = Plots.plot(title = "$(ider)\nxi: $ξstr, beta: $βstr", 
+            xlabel = "flx", ylabel = "pdf", 
+            legend = false, yaxis = nothing)
+        
+        Ch.Plots.plot_marginal!(p, boundle, ξ, β, [:fba, :hr, :ep], ider)
+        
+        pdf_maxval = Ch.Utils.pdf_maxval(boundle, ξ, β, [:ep, :fba], ider)
+        Plots.plot!(p, yaxis = [0.0, pdf_maxval * 1.2])
+        push!(ps, p)
+    end
+    p = Plots.plot(framestyle = :none, 
+        legend = :topleft, legendtitlefont = 15, legendtitle = "Toy model", legendfont = 15)
+    p = Plots.plot!(p, [],[], lw = 5, color = :blue, label = "FBA")
+    Plots.plot!(p, [],[], lw = 5, color = :red, label = "EP")
+    Plots.plot!(p, [],[], lw = 5, color = :red, label = "HR")
+    
+    push!(ps, p);
+    
+    return Plots.plot(ps..., size = [1000, 1000])
+end
+
+gif = Plots.@gif for i in eachindex(βs_)
+    plot1(iders_, ξ, βs_[i])
+end every 1
+
+# saving gif
+gif_file = "toy_model__all_marginals__varying_beta.gif"
+# cp("tmp.gif", gif_file, force = true)
+gif
+
+# +
+# Plot the marginals as function of ξs
+
+# Because increase xi implies a reduction in the intakes all fluxes of the network
+# tends to the closest to zero limit possible
+
+β = boundle.βs[5] # Select β to plot
+ξs_ = boundle.ξs # Select ξs to plot
+ξs_ = [ξs_; reverse(ξs_)]
+iders_ = metnet.rxns # Select rxn iders
+
+function plot2(iders, ξ, β, ξ0)
+    ps = []
+    
+    metnet0 = Ch.Utils.get_data(boundle, ξ0, :net)
+    
+    for ider in iders
+        ξstr = round(ξ, digits = 1)
+        βstr = round(β, digits = 1)
+        
+        p = Plots.plot(title = "$(ider)\nxi: $ξstr, beta: $βstr", 
+            xlabel = "flx", ylabel = "pdf", 
+            legend = false,
+            yaxis = nothing)
+        
+        Ch.Plots.plot_marginal!(p, boundle, ξ, β, [:fba, :hr, :ep], ider)
+        
+        lb_, ub_ = Ch.Utils.bounds(metnet0, ider)
+        margin_ = abs(ub_ - lb_) * 0.1
+        pdf_maxval = Ch.Utils.pdf_maxval(boundle, ξ, β, [:ep, :fba], ider)
+        Plots.plot!(p, 
+            xaxis = [lb_ - margin_, ub_ + margin_],
+            yaxis = [0.0, pdf_maxval * 1.2])
+        
+        push!(ps, p)
+    end
+    p = Plots.plot(framestyle = :none, 
+        legend = :topleft, legendtitlefont = 15, legendtitle = "Toy model", legendfont = 15)
+    p = Plots.plot!(p, [],[], lw = 5, color = :blue, label = "FBA")
+    Plots.plot!(p, [],[], lw = 5, color = :red, label = "EP")
+    Plots.plot!(p, [],[], lw = 5, color = :black, label = "HR")
+    
+    push!(ps, p);
+    
+    return Plots.plot(ps..., size = [1000, 1000])
+end
+
+# gif
+gif = Plots.@gif for i in eachindex(ξs_)
+    plot2(iders_, ξs_[i], β, minimum(ξs_))
+end every 1
+
+# saving gif
+gif_file = "toy_model__all_marginals__varying_xi.gif"
+# cp("tmp.gif", gif_file, force = true)
+gif
+
+# +
+# Plot stoi err asfunctio of xi
+β = boundle.βs[1] # Select β to plot
+
+ξs_ = boundle.ξs#[[1, 10, 30, 40, 50]] # Select ξs to plot
+
+p = Plots.plot(title = "Toy Model\nbeta: $β", 
+    xlabel = "xi", ylabel = "stoi err/ flx", 
+)
+
+iders_ = metnet.mets # Select iders to plot
+for ider in iders_
+    errs_ = [Ch.Utils.stoi_err(boundle, ξ, β, :ep, ider) for ξ in ξs_]
+    Plots.plot!(p, ξs_, errs_, label = "", color= :black)
+end
+Plots.plot!(p, [], [], color = :black, label = "abs stoi err", lw = 3)
+
+flxs_ = [abs.(Ch.Utils.av(boundle, ξ, β, :ep)) for ξ in ξs_]
+Plots.plot!(p, ξs_, mean.(flxs_), lw = 3, label = "", color = :white)
+Plots.plot!(p, ξs_, mean.(flxs_), ls = :dash, lw = 3, label = "mean abs flx", color = :blue)
+Plots.plot!(p, ξs_, minimum.(flxs_), lw = 3, label = "", color = :white)
+Plots.plot!(p, ξs_, minimum.(flxs_), ls = :dash, lw = 3, label = "min abs flx", color = :red)
+
+# gif (making a zoom)
+yulims_ = Ch.Utils.logspace(-1,1.2, 100) |> reverse
+yulims_ = [fill(maximum(yulims_), 25); yulims_]
+yulims_ = [reverse(yulims_); yulims_]
+gif = Plots.@gif for i in eachindex(yulims_)
+    Plots.plot!(p, yaxis = [-0.01, yulims_[i]])
+end every 1
+
+# saving gif
+gif_file = "toy_model__stoi_err_vs_xi.gif"
+# cp("tmp.gif", gif_file, force = true)
+gif
+# +
+# Plot stoi err asfunctio of xi
+ξ = boundle.ξs[10] # Select ξ to plot
+ξstr = round(ξ, digits = 2)
+βs_ = boundle.βs[2:end] # Select βs to plot
+
+p = Plots.plot(title = "Toy Model\nxi: $ξstr", 
+    xlabel = "beta", ylabel = "stoi err", 
+    xaxis = :log10,
+#     yaxis = [-1e-3, 1e-3]
+)
+
+iders_ = metnet.mets # Select iders to plot
+for ider in iders_
+    errs_ = [Ch.Utils.stoi_err(boundle, ξ, β, :ep, ider) for β in βs_]
+    Plots.plot!(p, βs_, errs_, color = :black, label = "")
+end
+
+Plots.plot!(p, [], [], color = :black, label = "abs stoi err")
+
+flxs_ = [abs.(Ch.Utils.av(boundle, ξ, β, :ep)) for β in βs_]
+Plots.plot!(p, βs_, mean.(flxs_), lw = 3, label = "", color = :white)
+Plots.plot!(p, βs_, mean.(flxs_), ls = :dash, lw = 3, label = "mean abs flx", color = :blue)
+Plots.plot!(p, βs_, minimum.(flxs_), lw = 3, label = "", color = :white)
+Plots.plot!(p, βs_, minimum.(flxs_), ls = :dash, lw = 3, label = "min abs flx", color = :red)
+
+# gif
+yulims_ = Ch.Utils.logspace(-0.5,0.9, 100) |> reverse
+yulims_ = [fill(maximum(yulims_), 25); yulims_]
+yulims_ = [reverse(yulims_); yulims_]
+gif = Plots.@gif for i in eachindex(yulims_)
+    Plots.plot!(p, yaxis = [-0.01, yulims_[i]])
+end every 1
+
+# saving gif
+gif_file = "toy_model__stoi_err_vs_beta.gif"
+# cp("tmp.gif", gif_file, force = true)
+gif
+# -
+
