b241d3ec412d35df52dcd1f6e6965ad002e3e6a4
josePereiro <jose.pereiro.lab@gmail.com>
2020-06-06 20:04:27 -0400

    Delete MetabolicEPUtils, test passed

--- src/Utils/MetabolicEPUtils/MetabolicEPUtils.jl
+++
@@ -1,9 +0,0 @@
-"""
-    This utilities are a simple interface 
-    to some methods from MetabolicEP
-"""
-
-
-include("metabolicEP.jl")
-include("hrsample.jl")
-
--- src/Utils/MetabolicEPUtils/hrsample.jl
+++
@@ -1,2 +0,0 @@
-hrsample(metnet::MetNet; drop_samples = true, kwargs...) = 
-    HRout(hrsample(metnet.S, metnet.b, metnet.lb, metnet.ub; kwargs...), drop_samples)
