a4ce9bfb713fba2cfee82e63ad178ab89ac4c21e
josePereiro <jose.pereiro.lab@gmail.com>
2020-06-11 14:42:52 -0400

    Add maxent_ep documentation

--- src/MaxEntEP/maxent_ep.jl
+++ src/MaxEntEP/maxent_ep.jl
@@ -1,6 +1,39 @@
 # Code derived from metabolicEP (https://github.com/anna-pa-m/Metabolic-EP)
 
-function maxent_ep(K::AbstractArray{T,2}, Y::Array{T,1}, lb::Array{T,1}, ub::Array{T,1};
+"""
+res=metabolicEP(S,b,lb,ub,...)
+
+
+The output in res is of type `EPout`: there are several fields:
+-   `μ::Vector`: A parameter linked to the mean of the posterior probability
+-   `σ::Vector`: A parameter linked to the std  of the posterior probability
+-   `av::Vector`: The mean posterior probability
+-   `va::Vector`: The variance of the posterior probability
+-   `sol::EPFields`: The internal field status. From this value we can restart the sampling from a specific state.
+-   `status::Symbol`: either ``:converged`` or ``:unconverged``.
+
+Input (required)
+----
+- `S`: MxN matrix (either sparse or dense) please note that if you input a dense version, the algorithm is slighlty more efficient. Dense matrices can be create from sparse ones with `Matrix(S)`.
+- `b`: a vector of M intakes/uptakes
+- `lb`: a vector of lengh N of lower bounds.
+- `ub`: a vector of lengh N of upper bounds.
+
+Input (optional arguments).
+----
+- `alpha` (inverse temperature::``Real``): default 10^7;  the zero temperature algorithm is run setting ``beta=Inf``.
+- `beta_vec` (MaxEnt distribution inverse temperature vector:: ``Vector{T}``): default ``T[]``
+- `verbose` (``true`` or ``false``): default ``true``
+- `damp` (∈ (0,1) newfield = damp * oldfield + (1-damp)* newfield): default 0.9
+- `epsconv` (convergence criterion): default 1e-6
+- `maxiter` (maximum number of iterations): default 2000
+- `maxvar`  (threshold on maximum variance): default 1e50
+- `minvar`  (threshold on minimum variance): default 1e-50
+- `solution` (start from solution. Is of type ``EPout``): default: ``nothing``
+- `expval` (fix to posterior probability of mean and/or variance to values): default ``nothing``. expval can be either at ``Tuple{Float64,Float64,Int}`` or a ``Vector{Tuple{Float64,Float64,Int}}``. Values can be fixed as``expval=(0.2,0.4,4)`` meaning that for flux index 4 the mean is set to 0.2 and the variance to 0.4. Fixing more values ``expval=[(0.2, 0.3, 4), (0.4, nothing, 5)]``: in this case, we fix the posterior of flux 4 to 0.2 (mean) and 0.3 (variance), while for flux 5 we fix the mean to 0.4 and we keep the variance free.
+"""
+
+function maxent_ep(S::AbstractArray{T,2}, b::Array{T,1}, lb::Array{T,1}, ub::Array{T,1};
     alpha::Real=1e7,      # inverse temperature
     beta_vec::Vector{T} = T[], # maxent inverse temperature vector
     verbose::Bool=true,  # output verbosity
@@ -23,16 +56,16 @@ function maxent_ep(K::AbstractArray{T,2}, Y::Array{T,1}, lb::Array{T,1}, ub::Arr
     The scalefactor is just the maximum absolute bound (lb or ub).
     If a solution is not given, the EPfield will be fresh
     =#
-    updatealg,scalefact,epfield = prepareinput(K,Y,llb,lub, alpha, verbose,solution,expval)
+    updatealg, scalefact, epfield = prepareinput(S,b,llb,lub, alpha, verbose,solution,expval)
 
     #=
     Scale down μ, s, av, va of epfield and lub, llb and Y using the 
     previous computed scalefactor.
     If epfield is fresh, it only will have effect on lub, llb and Y
     =#
-    scaleepfield!(epfield,lub,llb,Y,1.0/scalefact) # rescaling fields in [0,1]
+    scaleepfield!(epfield,lub,llb,b,1.0/scalefact) # rescaling fields in [0,1]
 
-    epmat = alpha < Inf ? EPMat(K, Y, llb, lub, alpha) : EPMatT0(K,Y,llb, lub)
+    epmat = alpha < Inf ? EPMat(S, b, llb, lub, alpha) : EPMatT0(S,b,llb, lub)
     beta_vec = prepare_βv(epmat, beta_vec)
     epalg = EPAlg(alpha, beta_vec, minvar, maxvar, epsconv, damp, maxiter, verbose)
 
@@ -41,7 +74,7 @@ function maxent_ep(K::AbstractArray{T,2}, Y::Array{T,1}, lb::Array{T,1}, ub::Arr
     returnstatus, iter = epconverge!(epfield, epmat, epalg, updatealg)
 
     #= Scale back μ, s, av, va of epfield and lub, llb and Y =#
-    scaleepfield!(epfield,lub,llb,Y,scalefact)
+    scaleepfield!(epfield,lub,llb,b,scalefact)
     if alpha < Inf
         return  EPout(epfield.μ, epfield.s, epfield.av, epfield.va, epfield, returnstatus, iter)
     else
