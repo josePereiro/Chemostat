df5b903818e7978f36063eeb2ab3c719451ecf42
josePereiro <jose.pereiro.lab@gmail.com>
2020-06-26 15:03:30 -0400

    Add fva_preprocess ignored kwarg

--- src/Utils/MetNetUtils/fva_preprocess.jl
+++ src/Utils/MetNetUtils/fva_preprocess.jl
@@ -1,12 +1,17 @@
 # Code derived from metabolicEP (https://github.com/anna-pa-m/Metabolic-EP)
 function fva_preprocess(S,b,lb,ub,rxns; 
-        verbose = false, eps = 0.0, upfrec = 10)
-    b,lb, ub = copy(b), copy(lb), copy(ub)
+        verbose = false, eps = 0.0, 
+        ignored = [], # rxns skip preprocess
+        upfrec = 10)
+        
+    b, lb, ub = copy(b), copy(lb), copy(ub)
     n = length(lb)
     ei = zeros(n)
     blocked = falses(n)
     upfrec = floor(Int, n/upfrec)
     for i=1:n
+
+        i in ignored && continue
         
         show_progress = verbose && (i == 1 || i % upfrec == 0 || i == n)
         show_progress && (print("fva_processing [$i / $n]        \r"); flush(stdout))
@@ -38,9 +43,14 @@ function fva_preprocess(S,b,lb,ub,rxns;
 end
 
 
-function fva_preprocess(metnet::MetNet; verbose = false, eps = 0.0, return_blocked = false,)
+function fva_preprocess(metnet::MetNet; 
+            verbose = false, eps = 0.0, return_blocked = false,
+            ignored = [] # rxns skip preprocess
+        )
+    ignored = map((r) -> rxnindex(metnet, r), ignored)
+
     S_, b_, lb_, ub_, rxns_, idxb_ = fva_preprocess(metnet.S, metnet.b, metnet.lb, 
-        metnet.ub, metnet.rxns; verbose = verbose, eps = eps);
+        metnet.ub, metnet.rxns; verbose = verbose, eps = eps, ignored = ignored);
     
     metnet = MetNet(metnet; S = S_, b = b_, lb = lb_, ub = ub_, rxns = rxns_)
     return return_blocked ? (metnet, idxb_) : metnet
