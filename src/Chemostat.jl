52ac1d8d63ce059a1d148a76a67418210cf170b9
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-30 18:06:26 -0400

    Add requires Plots

--- src/Chemostat.jl
+++ src/Chemostat.jl
@@ -1,5 +1,7 @@
 module Chemostat
 
+import Requires: @require
+
 include("Utils/Utils.jl")
 
 include("LP/LP.jl")
@@ -7,6 +9,8 @@ include("Test/Test.jl")
 include("SteadyState/SteadyState.jl")
 include("MaxEntEP/MaxEntEP.jl")
 include("MaxEntHR/MaxEntHR.jl")
-include("Plots/Plots.jl")
+@require Plots="4076af6c-e467-56ae-b986-b466b2749572" begin
+    include("Plots/Plots.jl")
+end
 
 end # module
