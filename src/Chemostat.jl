01bf53ea6334809eda750cdb606d4f1aff1ff352
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-31 00:07:34 -0400

    Remove requires, update Plots

--- src/Chemostat.jl
+++ src/Chemostat.jl
@@ -9,8 +9,6 @@ include("Test/Test.jl")
 include("SteadyState/SteadyState.jl")
 include("MaxEntEP/MaxEntEP.jl")
 include("MaxEntHR/MaxEntHR.jl")
-@require Plots="4076af6c-e467-56ae-b986-b466b2749572" begin
-    include("Plots/Plots.jl")
-end
+include("Plots/Plots.jl")
 
 end # module
