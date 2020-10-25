bbb7bcbbc06643ea7851533be1008fbad8298479
josePereiro <jose.pereiro.lab@gmail.com>
2020-10-25 17:57:22 -0400

    Add ecoli core test model

--- src/Test/Test.jl
+++ src/Test/Test.jl
@@ -7,9 +7,11 @@ module Test
     import ..Utils: MetNet, lb!, EPout, EPFields, expanded_model, 
                     Met, Rxn, set_met!, set_rxn!
     import ..LP: fva_preprocess
+    import MAT
 
     include("lineal_model.jl")
     include("toy_model.jl")
+    include("ecoli_core.jl")
     include("test_types.jl")
 
 end
