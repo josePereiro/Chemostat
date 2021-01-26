ee1c730885797c91a901683d0d9f9d1a10831e6d
josePereiro <jose.pereiro.lab@gmail.com>
2021-01-26 16:56:03 -0500

    Add plot_marginal normalization op

--- src/Plots/plot_marginal.jl
+++ src/Plots/plot_marginal.jl
@@ -3,23 +3,31 @@ const fba_color = :blue
 const hr_color = :black
 
 function plot_marginal!(p, μ::Real, σ::Real, lb::Real, ub::Real;
-        av = nothing, 
-        h = 1, lw = 5,
-        color = :black,
-        label = "", kwargs...)
-        
+    av = nothing, h = 1, lw = 5, color = :black,
+    label = "", normalize = false, kwargs...
+)
+    
     sσ = sqrt(σ)
     tN = Truncated(Normal(μ, sσ), lb, ub) 
     global_max_ = isnothing(av) ? clamp(μ, lb, ub) : av
     margin_ = abs(ub - lb) * 0.1
-    if sσ == 0.0 || !(-Inf < pdf(tN, global_max_) < Inf)
+    if sσ == 0.0 || isinf(pdf(tN, global_max_))
+        # delta
         plot!(p, [lb - margin_, ub + margin_], [0.0, 0.0]; 
-            label = "")
+            label = ""
+        )
         plot!(p, [global_max_, global_max_], [0.0, h]; 
-            lw, label, color, kwargs...)
+            lw, label, color, kwargs...
+        )
     else
-        plot!(p, x -> pdf(tN, x), lb - margin_, ub + margin_; 
-            lw, label, color, kwargs...)
+        # normal
+        xs = range(lb, ub; length = 1000)
+        ys = [pdf(tN, x) for x in xs]
+        ys .= !normalize ? ys ./ maximum(ys) : ys
+        plot!(p, xs, ys;
+            xlim = [lb - margin_, ub + margin_],
+            lw, label, color, kwargs...
+        )
     end
     return plot!(p; kwargs...)
 end
@@ -27,7 +35,8 @@ end
 # Single out
 function plot_marginal!(p, metnet::MetNet, out::Union{FBAout, EPout}, ider; 
         color = out isa FBAout ? fba_color : ep_color,
-        label = rxns(metnet, ider), kwargs...)
+        label = rxns(metnet, ider), kwargs...
+    )
         
     lb_ = lb(metnet, ider)
     ub_ = ub(metnet, ider)
@@ -39,6 +48,7 @@ function plot_marginal!(p, metnet::MetNet, out::Union{FBAout, EPout}, ider;
 end
 
 
+# TODO: add normalization option
 function plot_marginal!(p, metnet::MetNet, out::HRout, ider; 
         h = :ignored, color = hr_color, 
         label = rxns(metnet, ider), kwargs...)
@@ -49,7 +59,8 @@ function plot_marginal!(p, metnet::MetNet, out::HRout, ider;
     hist = hists(metnet, out, ider)
     plot!(p, normalize(hist, mode = :pdf); 
         xaxis = [lb_ - margin_, ub_ + margin_],
-        label, color, kwargs...)
+        label, color, kwargs...
+    )
 end
 
 function plot_marginal!(p, metnet::MetNet, outs::Vector, ider::IDER_TYPE; kwargs...)
@@ -65,7 +76,8 @@ end
 function plot_marginal!(p, bundle::ChstatBundle, 
         ξ::Real, β::Real, data_keys::Vector,
         ider::IDER_TYPE, metnet_data_key::Symbol = metnet_data_key; 
-        kwargs...)
+        kwargs...
+    )
 
     metnet = bundle[ξ, metnet_data_key]
     outs = collect_data(d -> d isa AbstractOut, bundle, ξ, β, data_keys)
