5469aff02f4e8bf3455d025d50dcd4cebfc3caf2
josePereiro <jose.pereiro.lab@gmail.com>
2021-05-02 03:02:20 -0400

    small change

--- src/Utils/Types/EPMat.jl
+++ src/Utils/Types/EPMat.jl
@@ -10,16 +10,16 @@ end
 
 function EPMat(S::AbstractArray{T}, b::AbstractArray{T}, 
         lb::AbstractArray{T}, ub::AbstractArray{T}, alpha::T) where T <: Real
-    alpha == Inf && error("For Inf alpha use 'EPMatT0'")
+    isinf(alpha) && error("For Inf alpha use 'EPMatT0'")
     M, N = size(S)
     αStS = sparse(alpha * S' * S)
     return EPMat(   
-                    #= αStS      =# αStS, 
-                    #= αStS_diag =# diag(αStS), 
-                    #= αStb      =# alpha * S' * b, 
-                    #= Σ         =# zeros(T,N,N), 
-                    #= v         =# zeros(T,N), 
-                    #= lb        =# lb, 
-                    #= ub        =# ub
-                )
+        #= αStS      =# αStS, 
+        #= αStS_diag =# diag(αStS), 
+        #= αStb      =# alpha * S' * b, 
+        #= Σ         =# zeros(T,N,N), 
+        #= v         =# zeros(T,N), 
+        #= lb        =# lb, 
+        #= ub        =# ub
+    )
 end
